21045734e8a1ba32df22e5774de70662a7aa79a4
nIKaaapav <nikapavlenko20.np@gmail.com>
2023-12-16 12:36:34 -0600

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || '3001'
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
-  await app.listen(3001, '0.0.0.0');
+  await app.listen(port);
   logger.log(`Server is running at ${await app.getUrl()}`);
 }
 bootstrap();
